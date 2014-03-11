f16878b0ded3f5669ff7f3ef69277e0ca20a84e5
Piotr Solnica <piotr.solnica@gmail.com>
2014-03-11 21:23:05 +0100

    Refactor spec

--- spec/unit/book_spec.rb
+++ spec/unit/book_spec.rb
@@ -3,39 +3,17 @@ require 'spec_helper'
 describe Book do
   subject(:book) { Book.new }
 
-  let(:page) { Page.new(1) }
-
-  describe '#page' do
-    before { book.add_page(page) }
-
-    context 'when page exists' do
-      it 'should return page' do
-        expect(book.page(1)).to be(page)
-      end
-    end
-
-    context 'when page does not exist' do
-      it 'should raise error' do
-        expect { book.page(2) }.to raise_error(
-          RuntimeError, "Book does not have a page with number: 2"
-        )
-      end
-    end
-  end
-
   describe '#add_page' do
+    let(:page) { Page.new(1, 'test') }
+
     it 'should return self' do
       expect(book.add_page(page)).to be(book)
     end
 
     it 'should add page to book' do
       book.add_page(page)
-      expect(book.pages).to include(page)
-    end
-
-    it 'should add page to book index' do
-      book.add_page(page)
-      expect(book.index).to include(1 => page)
+      expect(book.page(page.number)).to be(page)
     end
   end
+
 end
